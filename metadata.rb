c1a143636ffa45379d9bc69f6b76cbd123e4430e
Erik Thorselius <erik.thorselius@gmail.com>
2013-07-18 12:58:40 +0200

    Bumbed version.

--- metadata.rb
+++ metadata.rb
@@ -5,4 +5,4 @@ license          "All rights reserved"
 description      "Installs/Configures gitolite"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 depends          "git"
-version          "0.0.4"
+version          "0.0.5"
