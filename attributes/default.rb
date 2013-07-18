23afe10418ca6b30afe2ecf53326b8d86391fb4d
Erik Thorselius <erik.thorselius@gmail.com>
2013-07-18 12:56:32 +0200

    Updateded gitolite version.

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,2 +1,2 @@
-default['gitolite']['version'] = '3.04' # needs to match name of a tag or commit id on the gitolite repo
+default['gitolite']['version'] = '3.5.2' # needs to match name of a tag or commit id on the gitolite repo
 default['gitolite']['instances'] = []
